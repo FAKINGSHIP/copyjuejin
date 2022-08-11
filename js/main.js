9b12e605adf3c7606b55cdbc656d6f5fe495c0f4
blue-se <1973353768@qq.com>
2022-08-11 12:00:37 +0800

    实现搜索历史，但搜索框没绑定enter事件

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ var isopen2 = true;
    
 
     // 搜索框active
-    var getDiv3 = document.querySelector(".search-form");
+    var getDiv3 = document.querySelector(".search-input");
     getDiv3.addEventListener('click', function(){
         document.querySelector(".search-add").classList.add("active");
         document.querySelector(".search-add-ul").classList.add("active");
@@ -104,13 +104,17 @@ var isopen2 = true;
         document.querySelector(".search-form").classList.add("active");
         document.querySelector(".search-icon-container").classList.add("active");
         document.querySelector(".search-icon").setAttribute("src","https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/6dbcdb42ffe8d518a318a5e26efade18.svg");
-        // document.querySelector(".typehead").setAttribute("style","");
         document.querySelector(".search-input").setAttribute("placeholder","搜索文章/小册/标签/用户");
         document.querySelector(".creator-item").classList.add("hide");
         document.querySelector(".add-btn").classList.add("hide");
         document.querySelector(".more").classList.add("hide");
         document.querySelector(".notification").classList.add("hide");
         document.querySelector(".menu").classList.add("hide");
+        // 展开搜索历史
+        let localCon = localStorage.getItem('searches');
+        if(localCon){
+            document.querySelector(".typehead").setAttribute("style", "display:block");
+        }
     })
     document.addEventListener('click',function(){
         document.querySelector(".search-add").classList.remove("active");
@@ -119,13 +123,14 @@ var isopen2 = true;
         document.querySelector(".search-form").classList.remove("active");
         document.querySelector(".search-icon-container").classList.remove("active");
         document.querySelector(".search-icon").setAttribute("src","https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/1e8ab9a22f0ddc36349f60b38900d0bd.svg");
-        // document.querySelector(".typehead").setAttribute("style","display:none");
         document.querySelector(".search-input").setAttribute("placeholder","探索稀土掘金");
         document.querySelector(".creator-item").classList.remove("hide");
         document.querySelector(".add-btn").classList.remove("hide");
         document.querySelector(".more").classList.remove("hide");
         document.querySelector(".notification").classList.remove("hide");
-        document.querySelector(".menu").classList.remove("hide");   
+        document.querySelector(".menu").classList.remove("hide");  
+        // 折叠搜索历史
+        document.querySelector(".typehead").setAttribute("style", "display:none"); 
     })
     getDiv3.addEventListener('click',function(event){
         var e=event||window.event;
@@ -135,6 +140,47 @@ var isopen2 = true;
             e.stopPropagation();// 其余浏览器 阻止事件冒泡
         }
     });
+    var getDiv8 = document.querySelector(".search-icon-container");
+    var getDiv9 = document.querySelector(".clear");
+    var getDiv10 = document.querySelector(".list");
+    getDiv8.addEventListener('click', function() {
+        // 获取搜索栏的值
+        let userVal = search.value
+        let arr = []
+        console.log(userVal);
+        // 获取本地存储的内容
+        let localCon = localStorage.getItem('searches');
+        console.log(localCon);
+        arr = localCon ? arr = JSON.parse(localCon) : []
+        if (userVal) {
+            arr.unshift(userVal)
+        }
+        // 将数组转化成字符串对象,放到本地存储中
+        localStorage.setItem('searches', JSON.stringify(arr))
+            // 渲染数据到网页中
+        showLog()
+            // 清空搜索框
+        search.value = ''
+    })
+    
+    // 清空
+    getDiv9.addEventListener('click', function() {
+        localStorage.removeItem('searches')
+            // localStorage.clear()
+        showLog()
+            // searchList.innerHTML = ''
+    });
+    
+    // 页面加载的时候将本地存储的内容显示到页面
+    function showLog() {
+        let locals = JSON.parse(localStorage.getItem('searches')) || []
+        let htmllis = ''
+        for (let i = 0; i < locals.length; i++) {
+            htmllis += `<div data-index=${i}>${locals[i]}</div>`
+        }
+        getDiv10.innerHTML = htmllis
+    }
+
 //  !!!!!同时点击创作者中心和个人中心都会打开，sad
 
 // 下滑一定程度main-header消失,view-nav置顶
